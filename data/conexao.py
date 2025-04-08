2beb7fca65b6ae5607eb9c52da53b61744889a0b
VictorCyuvi20 <victoramancio20202@gmail.com>
2025-04-08 11:15:41 -0300

    ARRUMANDO ERRO

--- data/conexao.py
+++ data/conexao.py
@@ -3,7 +3,7 @@ import mysql.connector
 class Conexao:
 
     def  criar_conexao():
-        conexao = mysql.connector.connect(host = "bggodofredo-alexstocco-93db.b.aivencloud.com", 
+        conexao = mysql.connector.connect(host = "bdgodofredo-alexstocco-93db.b.aivencloud.com", 
                                           port = 27974, 
                                           user = "3ds", 
                                           password = "banana", 
